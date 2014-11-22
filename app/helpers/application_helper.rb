243dc106b2995f7e79ba0ad78a0d235a045d8ef2
Chris Fritz <chrisvfritz@gmail.com>
2014-11-22 18:46:49 -0500

    updated bootstrap classes for flash types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,9 @@ module ApplicationHelper
 
   def bootstrap_class_for(flash_type)
     case flash_type.to_sym
-      when :success then 'alert-success'
-      when :error   then 'alert-danger'
-      when :alert   then 'alert-block'
-      when :notice  then 'alert-info'
+      when :notice        then 'alert-info'
+      when :success       then 'alert-success'
+      when :error, :alert then 'alert-danger'
       else flash_type.to_s
     end
   end
