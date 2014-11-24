cc5f5e0f9da8fc45049625754a137826c244cd3b
Chris Fritz <chrisvfritz@gmail.com>
2014-11-24 14:27:24 -0500

    updated requested scope for github and stack overflow omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,12 +4,12 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github,
     Rails.application.secrets[:omniauth][:github][:key],
     Rails.application.secrets[:omniauth][:github][:secret],
-    scope: 'user:email'
+    scope: 'user:email,gist,notifications'
 
   provider :stackexchange,
     Rails.application.secrets[:omniauth][:stackexchange][:id],
     Rails.application.secrets[:omniauth][:stackexchange][:secret],
     public_key: Rails.application.secrets[:omniauth][:stackexchange][:key],
     site: 'stackoverflow',
-    scope: 'no_expiry'
+    scope: 'no_expiry,read_inbox'
 end
