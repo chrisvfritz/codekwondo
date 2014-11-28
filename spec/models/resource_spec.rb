3b891a92d052152a773edbb80ffcd09023688456
Chris Fritz <chrisvfritz@gmail.com>
2014-11-28 16:19:47 -0500

    added url validations for resources and project completions

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -19,4 +19,36 @@ describe Resource do
     it { should be_valid }
   end
 
+  context 'when passed a valid URL WITHOUT http or https' do
+    before(:each) { @resource = create :resource, url: 'google.com' }
+
+    it 'should prepend http:// to the url' do
+      expect(@resource.url).to eq('http://google.com')
+    end
+  end
+
+  context 'when passed a URL that is NOT formatted properly' do
+    it 'should return a message saying it is NOT a valid web address' do
+      expect {
+        create :resource, url: 'arst'
+      }.to raise_exception(ActiveRecord::RecordInvalid, "Validation failed: Url isn't a valid web address")
+    end
+  end
+
+  context 'when passed a URL that\'s formatted properly but has a domain that does NOT exist on the web' do
+    it 'should return a message saying it does NOT exist on the web' do
+      expect {
+        create :resource, url: 'oih1235oieh1235oihe12io3e5hoi1.com'
+      }.to raise_exception(ActiveRecord::RecordInvalid, "Validation failed: Url doesn't seem to exist on the web")
+    end
+  end
+
+  context 'when passed a URL that\'s formatted properly, with a valid domain, but does not have a page' do
+    it 'should return a message saying that there is NOT a page on that domain' do
+      expect {
+        create :resource, url: 'www.google.com/not_a_page'
+      }.to raise_exception(ActiveRecord::RecordInvalid, "Validation failed: Url doesn't appear to be a page on www.google.com (404: Not Found)")
+    end
+  end
+
 end
