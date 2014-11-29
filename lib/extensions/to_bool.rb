a10cd47dcd07fa93ea6d6cf7d87943e8026f191d
Chris Fritz <chrisvfritz@gmail.com>
2014-11-28 21:14:45 -0500

    added more specs for to_bool extension

--- lib/extensions/to_bool.rb
+++ lib/extensions/to_bool.rb
@@ -3,20 +3,20 @@ module Extensions::ToBool
   def to_bool(nil_value = false)
     compare_value = self.class == String ? self.downcase : self
     case compare_value
-    when "no","false",false, "0", 0 then false
-    when "yes","true",true, "1", 1  then true
-    when nil                        then nil_value
-    else                                 !!compare_value
+    when 'no','false',false,'0',0,'' then return false
+    when 'yes','true',true,'1',1     then return true
+    when nil                         then return nil_value
+    else                                  return !!compare_value
     end
   end
 
   def is_bool?(nil_value = false)
     compare_value = self.class == String ? self.downcase : self
     case compare_value
-    when "no","false",false, "0", 0 then true
-    when "yes","true",true, "1", 1  then true
-    when nil                        then nil_value
-    else                                 false
+    when 'no','false',false,'0',0,'' then return true
+    when 'yes','true',true,'1',1     then return true
+    when nil                         then return nil_value
+    else                                  return false
     end
   end
 
--- spec/lib/extensions/to_bool_spec.rb
+++ spec/lib/extensions/to_bool_spec.rb
@@ -3,47 +3,55 @@ describe Extensions::ToBool do
   describe '#to_bool' do
 
     it 'should return false when "no"' do
-      expect( 'no'.to_bool ).to eq(false)
+      expect( 'no'.to_bool ).to be_a(FalseClass)
     end
 
     it 'should return false when "false"' do
-      expect( 'false'.to_bool ).to eq(false)
+      expect( 'false'.to_bool ).to be_a(FalseClass)
     end
 
     it 'should return false when false' do
-      expect( false.to_bool ).to eq(false)
+      expect( false.to_bool ).to be_a(FalseClass)
     end
 
     it 'should return false when "0"' do
-      expect( '0'.to_bool ).to eq(false)
+      expect( '0'.to_bool ).to be_a(FalseClass)
     end
 
     it 'should return false when 0' do
-      expect( 0.to_bool ).to eq(false)
+      expect( 0.to_bool ).to be_a(FalseClass)
+    end
+
+    it 'should return false with a blank string' do
+      expect( ''.to_bool ).to be_a(FalseClass)
     end
 
     it 'should return true when "yes"' do
-      expect( 'yes'.to_bool ).to eq(true)
+      expect( 'yes'.to_bool ).to be_a(TrueClass)
     end
 
     it 'should return true when "true"' do
-      expect( 'true'.to_bool ).to eq(true)
+      expect( 'true'.to_bool ).to be_a(TrueClass)
     end
 
     it 'should return true when true' do
-      expect( true.to_bool ).to eq(true)
+      expect( true.to_bool ).to be_a(TrueClass)
     end
 
     it 'should return true when "1"' do
-      expect( '1'.to_bool ).to eq(true)
+      expect( '1'.to_bool ).to be_a(TrueClass)
     end
 
     it 'should return true when 1' do
-      expect( 1.to_bool ).to eq(true)
+      expect( 1.to_bool ).to be_a(TrueClass)
+    end
+
+    it 'should return true with a non-empty string' do
+      expect( 'arst'.to_bool ).to be_a(TrueClass)
     end
 
     it 'should return false when nil' do
-      expect( nil.to_bool ).to eq(false)
+      expect( nil.to_bool ).to be_a(FalseClass)
     end
 
   end
@@ -51,47 +59,55 @@ describe Extensions::ToBool do
   describe '#is_bool?' do
 
     it 'should return true when "no"' do
-      expect( 'no'.is_bool? ).to eq(true)
+      expect( 'no'.is_bool? ).to be_a(TrueClass)
     end
 
     it 'should return true when "false"' do
-      expect( 'false'.is_bool? ).to eq(true)
+      expect( 'false'.is_bool? ).to be_a(TrueClass)
     end
 
     it 'should return true when false' do
-      expect( false.is_bool? ).to eq(true)
+      expect( false.is_bool? ).to be_a(TrueClass)
     end
 
     it 'should return true when "0"' do
-       expect( '0'.is_bool? ).to eq(true)
+      expect( '0'.is_bool? ).to be_a(TrueClass)
     end
 
     it 'should return true when 0' do
-       expect( 0.is_bool? ).to eq(true)
+      expect( 0.is_bool? ).to be_a(TrueClass)
+    end
+
+    it 'should return true with a blank string' do
+      expect( ''.is_bool? ).to be_a(TrueClass)
     end
 
     it 'should return true when "yes"' do
-       expect( 'yes'.is_bool? ).to eq(true)
+      expect( 'yes'.is_bool? ).to be_a(TrueClass)
     end
 
     it 'should return true when "true"' do
-       expect( 'true'.is_bool? ).to eq(true)
+      expect( 'true'.is_bool? ).to be_a(TrueClass)
     end
 
     it 'should return true when true' do
-       expect( true.is_bool? ).to eq(true)
+      expect( true.is_bool? ).to be_a(TrueClass)
     end
 
     it 'should return true when "1"' do
-       expect( '1'.is_bool? ).to eq(true)
+      expect( '1'.is_bool? ).to be_a(TrueClass)
     end
 
     it 'should return true when 1' do
-       expect( 1.is_bool? ).to eq(true)
+      expect( 1.is_bool? ).to be_a(TrueClass)
     end
 
     it 'should return false when nil' do
-       expect( nil.is_bool? ).to eq(false)
+      expect( nil.is_bool? ).to be_a(FalseClass)
+    end
+
+    it 'should return false with a non-empty string' do
+      expect( 'arst'.is_bool? ).to be_a(FalseClass)
     end
 
   end
