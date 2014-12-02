5186a4c8e449e74da977184244cb6f960c86860c
Chris Fritz <chrisvfritz@gmail.com>
2014-12-01 21:13:13 -0500

    simplified prereq validations

--- app/models/concerns/prereq_validations.rb
+++ app/models/concerns/prereq_validations.rb
@@ -6,18 +6,12 @@ module PrereqValidations
   end
 
   def is_acyclic
-    if self.send(self.class::PREREQ_MODEL) == self.prereq
-      return errors.add(:base, "A self can't be a prerequisite to itself")
-    end
+    return errors.add(:base, "A self can't be a prerequisite to itself") if self.send(self.class::PREREQ_MODEL) == self.prereq
 
     check_for_self = Proc.new do |current_self|
-      if self.send(self.class::PREREQ_MODEL) == current_self
-        return errors.add(:base, "Catch 22 detected. \"#{self.send(self.class::PREREQ_MODEL).title}\" is already required before \"#{self.prereq.title}\".")
-      end
+      return errors.add(:base, "Catch 22 detected. \"#{self.send(self.class::PREREQ_MODEL).title}\" is already required before \"#{self.prereq.title}\".") if self.send(self.class::PREREQ_MODEL) == current_self
 
-      current_self.prereqs.each do |self_to_check|
-        check_for_self.call self_to_check
-      end
+      current_self.prereqs.each { |self_to_check| check_for_self.call(self_to_check) }
     end
 
     check_for_self.call self.prereq
