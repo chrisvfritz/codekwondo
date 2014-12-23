313b19a65c5189db693c4a835e26e78fa1d2cdf4
Chris Fritz <chrisvfritz@gmail.com>
2014-12-23 16:09:04 -0500

    refactored bootstrap alert class helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,11 @@ module ApplicationHelper
   # BOOTSTRAP
   # ---------
 
-  def bootstrap_class_for(flash_type)
+  def bootstrap_alert_class_for(flash_type)
     case flash_type.to_sym
       when :notice        then 'alert-info'
-      when :success       then 'alert-success'
       when :error, :alert then 'alert-danger'
-      else flash_type.to_s
+      else "alert-#{flash_type}"
     end
   end
 
