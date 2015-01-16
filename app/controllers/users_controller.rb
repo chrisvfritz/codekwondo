13d735d6da73fdf3510a7a9fe96012acf9ef2f9f
Chris Fritz <chrisvfritz@gmail.com>
2015-01-16 10:18:58 -0500

    added error message for stack exchange accounts not connected to stack overflow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,7 @@ class UsersController < ApplicationController
   def connect_stackoverflow
     current_user.connect_stackoverflow(request.env['omniauth.auth'])
     redirect_to root_url, notice: 'StackOverflow account connected!'
+  rescue OmniAuth::Strategies::StackExchange::NotRegisteredForStackExchangeSiteError
+    redirect_to rool_url, alert: 'Your Stack Exchange account has never been used to log in to Stack Overflow. This must be done before connecting to Codekwondo.'
   end
 end
